eae8fd44557ef312f6ed2431237f0fd2aa3241e8
jsami <rj.sami21@outlook.com>
2016-09-23 21:23:40 +0400

    fix semantic

--- ch01/ex02.scm
+++ ch01/ex02.scm
@@ -1,6 +1,6 @@
 #!/usr/bin/petite --script
 
-;; Ben Bitdiddle order evaluation test
+;; Ben Bitdiddle evaluation order test
 
 (define (p) (p))
 
@@ -11,8 +11,8 @@
 
 (test 0 (p))
 
-;; If the interpreter uses Applicative-order => the test won't finish since in this behavior, the interpreter 
-;; evaluate each sub expression before applying the operation, so it will tend to evaluate `p` indefinitely
+;; If the interpreter uses Applicative-order => the test won't finish since in this way of evaluation, the interpreter 
+;; evaluates each sub expression before applying the operation, so it will tend to evaluate `p` indefinitely
 
 ;; If the interpreter uses Normal-order evaluation => the result will be 0 since the interpreter won't need to
 ;; evaluate `p` (expansion phase). In the evaluation phase, the if special form will be evaluated first, which will
